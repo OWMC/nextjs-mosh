d8d137c22cc8535e70da71994e9f6c198bda31a8
Oliver Wieland <contact@owmc.co.uk>
2024-11-07 11:15:38 +0000

    [TJH-23] Customise radixui theme.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import "@radix-ui/themes/styles.css";
+import './theme-config.css';
+import "./globals.css";
+import { Inter } from 'next/font/google';
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
-import { Theme } from '@radix-ui/themes'
+import { Theme, ThemePanel } from '@radix-ui/themes'
 import NavBar from "./Navbar/NavBar";
 
 const geistSans = localFont({
@@ -14,6 +16,11 @@ const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
+});
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
 });
 
 export const metadata: Metadata = {
@@ -28,11 +35,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Theme>
-          <NavBar />
+      <body className={inter.variable}>
+        <Theme accentColor="bronze" radius="small">
+            <NavBar />
           <main className="p-4">
             {children}
           </main>
